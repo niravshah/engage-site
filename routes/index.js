2adf76316a5d91802ac40544a5af265c3b1ede83
niravshah <nirav.shah83@gmail.com>
2016-07-10 11:31:03 +0100

    Home Redirection

--- routes/index.js
+++ routes/index.js
@@ -3,26 +3,18 @@ var router = express.Router();
 
 var content = {};
 
-content['en'] = {head : 'EMPLOYEE ENGAGEMENT'}
-content['ro'] = {head : 'IMPLICAREA ANGAJAȚILOR'}
+content['en'] = {s1_heading_1 : 'EMPLOYEE ENGAGEMENT'}
+content['ro'] = {s1_heading_1 : 'IMPLICAREA ANGAJAȚILOR'}
 
 router.get('/', function(req, res) {
-  if(host.indexOf('ro') > -1){
-    res.render('index', content['ro']);
-  }else{
-    res.render('index', content['en']);
-  }
-
+  res.redirect('/home')
 });
 
 router.get('/home', function(req, res) {
   var host = '' + req.get('host');
-  console.log("HOST:", host);
-  console.log("Index:", host.indexOf('ro'));
   if(host.indexOf('ro') > -1){
     res.render('index', content['ro']);
   }else{
-    console.log(content['en'])
     res.render('index', content['en']);
   }
 
