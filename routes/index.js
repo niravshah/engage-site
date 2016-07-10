389c1f7bd2f119bd38d4f1e3ca93c8635a93f90b
niravshah <nirav.shah83@gmail.com>
2016-07-10 11:26:05 +0100

    Mailjet Integration

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,26 @@ var router = express.Router();
 
 var content = {};
 
-content['en'] = {'s1.heading.1' : 'EMPLOYEE ENGAGEMENT'}
-content['ro'] = {'s1.heading.1' : 'IMPLICAREA ANGAJAȚILOR'}
+content['en'] = {head : 'EMPLOYEE ENGAGEMENT'}
+content['ro'] = {head : 'IMPLICAREA ANGAJAȚILOR'}
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  if(host.indexOf('ro') > -1){
+    res.render('index', content['ro']);
+  }else{
+    res.render('index', content['en']);
+  }
+
 });
 
 router.get('/home', function(req, res) {
-  console.log("HOST:", req.get('host'));
-
-  if(req.get('host').indexOf('ro')>-1){
+  var host = '' + req.get('host');
+  console.log("HOST:", host);
+  console.log("Index:", host.indexOf('ro'));
+  if(host.indexOf('ro') > -1){
     res.render('index', content['ro']);
   }else{
+    console.log(content['en'])
     res.render('index', content['en']);
   }
 
