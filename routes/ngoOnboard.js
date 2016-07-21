34e1c80c4312cca352d5ccd61cdd5cc8132f1777
niravshah <nirav.shah83@gmail.com>
2016-07-21 13:11:52 +0100

    S3 Integration

--- routes/ngoOnboard.js
+++ routes/ngoOnboard.js
@@ -1,34 +1,36 @@
 var express = require('express');
 var router = express.Router();
-var multer  = require('multer');
+var multer = require('multer');
 var multerS3 = require('multer-s3')
 var aws = require('aws-sdk');
 var Ngo = require('../models/ngo');
 
-var s3 = new aws.S3({params: {Bucket: 'engage-site-nns'}})
+var s3 = new aws.S3({signatureVersion: 'v4'});
 aws.config.region = 'eu-central-1';
-aws.config.update({accessKeyId: 'AKIAIL7X6PP22KAAN7JQ', secretAccessKey: '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'});
-/*var storage = multerS3({
- s3: s3,
- bucket: 'engage-site-nns',
- metadata: function (req, file, cb) {
- cb(null, {fieldName: file.fieldname});
- },
- key: function (req, file, cb) {
- cb(null, Date.now().toString())
- }
- });*/
-
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/')
+aws.config.update({accessKeyId: 'AKIAJ4D2THKELCUJJEHQ', secretAccessKey: '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'});
+var storage = multerS3({
+    s3: s3,
+    bucket: 'engage-site-nns',
+    acl: 'public-read',
+    metadata: function (req, file, cb) {
+        cb(null, {fieldName: file.fieldname});
     },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + '-'+ file.originalname)
+    key: function (req, file, cb) {
+        //cb(null, Date.now().toString())
+        cb(null, Date.now() + '-'+ file.originalname);
     }
 });
 
-var upload = multer({ storage: storage });
+/*var storage = multer.diskStorage({
+ destination: function (req, file, cb) {
+ cb(null, 'uploads/')
+ },
+ filename: function (req, file, cb) {
+ cb(null, Date.now() + '-'+ file.originalname)
+ }
+ });*/
+
+var upload = multer({storage: storage});
 
 router.get('/ngo/onboard', function (req, res) {
     var host = '' + req.get('host');
@@ -40,29 +42,28 @@ router.get('/ngo/onboard', function (req, res) {
 
 });
 
-router.post('/ngo/onboard/section1', upload.array('file[]'), function (req, res, next) {
+router.post('/ngo/onboard/section1', upload.array('file[]'), function (req, res) {
 
     console.log('Multer', req.files, req.body);
 
-    for(var i=0;i<req.files.length;i++){
-        console.log('For Loop',req.files[i].originalname, req.body.addData.logo)
-        if(req.files[i].originalname == req.body.addData.logo){
-            req.body.addData.logo = "/" + req.files[i].path
-        }else if(req.files[i].originalname == req.body.addData.banner){
-            req.body.addData.banner = "/" + req.files[i].path
+    for (var i = 0; i < req.files.length; i++) {
+        console.log('For Loop', req.files[i].originalname, req.body.addData.logo)
+        if (req.files[i].originalname == req.body.addData.logo) {
+            req.body.addData.logo = req.files[i].location
+        } else if (req.files[i].originalname == req.body.addData.banner) {
+            req.body.addData.banner = req.files[i].location
         }
     }
 
     req.body.addData.sname = req.body.addData.sname.toLowerCase().replace(/ /g, '-')
 
     var newNgo = new Ngo(req.body.addData);
-    newNgo.save(function(err,ngo){
-        if(err) res.status(500).json({"Error": err})
+    newNgo.save(function (err, ngo) {
+        if (err) res.status(500).json({"Error": err})
         else res.json(ngo)
     });
 
 });
-
 
 
 module.exports = router;
