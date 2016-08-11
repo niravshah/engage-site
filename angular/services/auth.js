443d882431f39f8415f52ce87c925bf28c29b6c0
niravshah <nirav.shah83@gmail.com>
2016-08-11 13:01:18 +0100

    Bug Fix with Auth Service

--- angular/services/auth.js
+++ angular/services/auth.js
@@ -33,8 +33,8 @@ app.service('AuthService', ['$http', '$window', 'jwtHelper',
 
         this.validToken = function () {
             var token = this.getToken();
-            var date = jwtHelper.getTokenExpirationDate(token);
             if (typeof token != 'undefined'){
+                var date = jwtHelper.getTokenExpirationDate(token);
                 if(date.setHours(0,0,0,0) < new Date().setHours(0,0,0,0)){
                     return false;
                 }else{
